6e23b0480a4c4bb41ad8d86662833cfd38cbf79a
Thiago MAFFEI <thiago.maffei@espci.fr>
2023-01-27 13:05:03 +0100

    Initial Commit

---
+++ cours1pa/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cours1pa
+//
+//  Created by Thiago Petrilli Maffei Dardis on 27/01/2023.
+//  Copyright © 2023 Thiago Petrilli Maffei Dardis. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
